b16dca280c814225fb483827b073dba9b212c068
Yuri Kletsun <ykletsun@lyonscg.com>
2017-10-11 17:48:29 +0300

    right logic

--- js/app.js
+++ js/app.js
@@ -27,6 +27,40 @@ function hexToRgb(hex) {
     return h;
 }
 
+function RGB2HSV(color){
+    var var_R = ( color[0] / 255 )
+    var var_G = ( color[1] / 255 )
+    var var_B = ( color[2] / 255 )
+
+    var var_Min = Math.min( var_R, var_G, var_B )    //Min. value of RGB
+    var var_Max = Math.max( var_R, var_G, var_B )    //Max. value of RGB
+    var del_Max = var_Max - var_Min             //Delta RGB value
+
+    var L = ( var_Max + var_Min )/ 2
+
+    if ( del_Max == 0 )                     //This is a gray, no chroma...
+    {
+        var H = 0
+        var S = 0
+    }
+    else                                    //Chromatic data...
+    {
+        if ( L < 0.5 ) S = del_Max / ( var_Max + var_Min )
+        else           S = del_Max / ( 2 - var_Max - var_Min )
+
+        var del_R = ( ( ( var_Max - var_R ) / 6 ) + ( del_Max / 2 ) ) / del_Max
+        var del_G = ( ( ( var_Max - var_G ) / 6 ) + ( del_Max / 2 ) ) / del_Max
+        var del_B = ( ( ( var_Max - var_B ) / 6 ) + ( del_Max / 2 ) ) / del_Max
+
+        if      ( var_R == var_Max ) H = del_B - del_G
+        else if ( var_G == var_Max ) H = ( 1 / 3 ) + del_R - del_B
+        else if ( var_B == var_Max ) H = ( 2 / 3 ) + del_G - del_R
+
+        if ( H < 0 ) H += 1
+        if ( H > 1 ) H -= 1
+    }
+    return [H, S, L];
+}
 
 /**
  *
@@ -46,6 +80,10 @@ function prepareVarsArray (arrayOfVars) {
     return preparedVarsArray;
 }
 
+function calcDistance(x, y, z) {
+    return Math.sqrt(x*x*100+y*y+z*z);
+}
+
 /**
  *
  * @param givenColorRgb
@@ -54,9 +92,16 @@ function prepareVarsArray (arrayOfVars) {
  */
 function getArrayOfDistances (givenColorRgb, preparedVarsArray) {
     var distances = [];
+    var givenColorHSB = RGB2HSV(givenColorRgb);
+    var currentColorHSB = "";
     for (var i = 0; i < preparedVarsArray.length - 1; i++) {
         var currentRgbColor = preparedVarsArray[i];
-        distances.push(Math.sqrt((givenColorRgb[0] - currentRgbColor[0])*(givenColorRgb[0] - currentRgbColor[0]) + (givenColorRgb[1] - currentRgbColor[1])*(givenColorRgb[1] - currentRgbColor[1]) + (givenColorRgb[2] - currentRgbColor[2])*(givenColorRgb[2] - currentRgbColor[2])));
+        currentColorHSB = RGB2HSV(currentRgbColor);
+        distances.push(
+            calcDistance(givenColorHSB[0] - currentColorHSB[0],
+                        givenColorHSB[1] - currentColorHSB[1],
+                        givenColorHSB[2] - currentColorHSB[2])
+        );
     }
     return distances;
 }
